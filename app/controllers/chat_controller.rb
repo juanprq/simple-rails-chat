7894c33a66772f82dfcb1779e1ba5a4f8856aeb1
Juan Pablo Ramírez <juan.ramirez.q@gmail.com>
2015-06-11 15:23:53 -0500

    rabbit mq example

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -4,27 +4,28 @@ class ChatController < ApplicationController
   def connection
 
     hijack do |tubesock|
-      # create a thread to maintain the connection with the client.
-      chat_thread = Thread.new do
+      connection = Bunny.new
+      connection.start
+      channel = connection.create_channel
+      fanout = channel.fanout 'chat'
 
-        # subscribe to redis
-        Redis.new.subscribe 'chat' do |on|
-
-          # when redis receive a message, send the message to the socket
-          on.message do |channel, message|
-            tubesock.send_data message
-          end
+      tubesock.onopen do
+        queue = channel.queue("", :exclusive => true)
+        queue.bind(fanout)
+        queue.subscribe block: false do |delivery_info, properties, body|
+          tubesock.send_data body
         end
       end
 
-      # when the socket receives a message, publish to redis
+      # when the socket receives a message, publish to rabbit
       tubesock.onmessage do |message|
-        Redis.new.publish 'chat', message
+        # channel.default_exchange.publish(message, :routing_key => queue.name)
+        fanout.publish message
       end
 
-      # if the socket closes the connection, kill the thread
+      # if the socket closes the connection
       tubesock.onclose do
-        chat_thread.kill
+        connection.close
       end
     end
   end

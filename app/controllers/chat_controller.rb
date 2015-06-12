6df5dfabd23db4d52ae0af5a5124d1301bb71703
Juan Pablo Ramírez <juan.ramirez.q@gmail.com>
2015-06-12 14:21:52 -0500

    change the model to work with 2 queues

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -7,45 +7,45 @@ class ChatController < ApplicationController
       connection = Bunny.new
       connection.start
       channel = connection.create_channel
-      fanout = nil
 
       # public working queue
       public_queue = channel.queue 'chat-public-queue'
 
       # when the socket receives a message, publish to rabbit
       tubesock.onmessage do |message|
-        request = JSON.parse message
+        request        = JSON.parse message
+        queue_out_name = "#{request['email']}-out"
+        queue_in_name  = "#{request['email']}-in"
+
+        queue_in  = channel.queue queue_in_name
+        queue_out = channel.queue queue_out_name
 
         # check the type
         case request['type']
         when 'open'
-          # create de fanout
-          fanout = channel.fanout request['email'], auto_delete: true
+          # send to the public queue
+          request.merge! ({
+            type:      'subscribe',
+            queue_out: queue_out_name,
+            queue_in:  queue_in_name,
+            agent:     '+573113412790',
+          })
 
-          queue = channel.queue('', :exclusive => true)
-          queue.bind(fanout)
-          queue.subscribe block: false do |delivery_info, properties, body|
-            # send message to the client chat
-            request = JSON.parse body
-
-            message = "#{request['name']}: #{request['message']}"
+          # create and subscribe to the in queue
+          queue_in.subscribe block: false do |delivery_info, properties, body|
+            # send the messages to the chat window
+            values = JSON.parse body
+            message = "#{values['name']}: #{values['message']}"
             tubesock.send_data message
           end
 
+          # ask cli consumer to subscribe to queues
+          public_queue.publish request.to_json
           # send message to the user
-          tubesock.send_data 'server: connected'
-
-          # send to the public queue
-          request = {
-            type:    'subscribe',
-            fanout:  fanout.name,
-            agent:   '+573113412790',
-            request: request
-          }
-          channel.default_exchange.publish(request.to_json, :routing_key => public_queue.name)
+          queue_in.publish({name: 'Server', message: 'connected'}.to_json)
         when 'message'
-          # simple message
-          fanout.publish message
+          # when the chat window sends a message, send to the cli
+          queue_out.publish message
         end
       end
 
